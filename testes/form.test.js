fd252e6d012d399046d5e1cab11c5b14ab4006aa
edgarlengo <pascoaledgar21@gmail.com>
2025-12-31 00:36:42 +0100

    Update form.test.js

--- testes/form.test.js
+++ testes/form.test.js
@@ -1,1 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('Cadastrar livro com sucesso', async ({ page }) => {
+  await page.goto('file://' + process.cwd() + '/index.html');
+
+  await page.fill('#titulo', 'Livro de Teste');
+  await page.fill('#autor', 'Autor Teste');
+
+  await page.click('#btnCadastrar');
+
+  const mensagem = page.locator('#mensagem');
+  await expect(mensagem).toContainText('Livro cadastrado');
+});
 
